d423c6bcf3e63f863de9b2759d062c44db016f49
bthiaw <bthiaw@gmail.com>
2026-02-13 12:50:38 +0000

    Adding custom filter in task02_interface_configuration.py

--- task02_interface_configuration.py
+++ task02_interface_configuration.py
@@ -3,8 +3,19 @@ import yaml
 
 ENV = Environment(loader=FileSystemLoader('.'))
 
-template = ENV.get_template("task02_interface_template.j2")
+def get_interface_speed(interface_name):
+    """ get_interface_speed returns the default Mbps value for a given
+        network interface by looking for certain keywords in the name
+    """
 
+    if 'gigabit' in interface_name.lower():
+        return 1000
+    if 'fast' in interface_name.lower():
+        return 100
+
+ENV.filters['get_interface_speed'] = get_interface_speed  
+
+template = ENV.get_template("task02_interface_template.j2")
 
 with open("task02_interface_data.yml") as f:
     interfaces = yaml.safe_load(f)
